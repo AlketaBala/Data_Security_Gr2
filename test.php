8112eca3833be0d0d0a2a15500b79d5eff0aea72
AlketaBala <93828279+AlketaBala@users.noreply.github.com>
2022-04-17 22:44:49 +0200

    Vendosja e stileve të tekstit

---
+++ test.php
@@ -0,0 +1,15 @@
+<html lang="en">
+<head>
+  
+<style> 
+
+input[type=text] {
+  width: 30%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  box-sizing: border-box;
+  border: none;
+  background-color: lightblue;
+  color: white;
+
+}
