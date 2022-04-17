8fe1beb3ec4220fb7141a3fbcb4eca323985aa9f
AlbinHashani <94253491+AlbinHashani@users.noreply.github.com>
2022-04-17 23:43:48 +0200

    Update forma.php

--- forma.php
+++ forma.php
@@ -12,6 +12,37 @@ input[type=text] {
   color: white;
 }
 
+
+
   </style>
 </head>
 <body>
+<h2>Forma per testim</h2>
+
+
+<form action="test.php" method="GET">
+
+  
+
+  <label for="fname">Zgjedhe file te cilen te cilen deshiron ta perdoresh si çeles:</label><br>
+
+  <input type="text" id="flibri" name="flibri"><br>
+
+  <label for="fname">Shkruje Plaintext:</label><br>
+
+  <input type="text" id="fname" name="fname"><br>
+
+ 
+
+  <input type="submit" value="Submit">
+
+
+</form> 
+
+
+
+</body>
+
+</html>
+
+
