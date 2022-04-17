efb3a949156379cb413e1e836366ab672a522c9a
AlketaBala <93828279+AlketaBala@users.noreply.github.com>
2022-04-17 23:39:45 +0200

    Vendosja e stileve për forma.php

---
+++ forma.php
@@ -0,0 +1,17 @@
+<html lang="en">
+<head>
+  
+<style> 
+input[type=text] {
+  width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  box-sizing: border-box;
+  border: none;
+  background-color: #3CBC8D;
+  color: white;
+}
+
+  </style>
+</head>
+<body>
